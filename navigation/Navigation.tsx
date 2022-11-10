2aee7c5190f96f6cebefa3e18fa05a016e650396
Holovanets <vip.golovanets@gmail.com>
2022-11-10 18:42:34 +0200

    Added route listener

--- navigation/Navigation.tsx
+++ navigation/Navigation.tsx
@@ -1,7 +1,29 @@
-import { FC } from 'react'
+import { useNavigationContainerRef } from '@react-navigation/native'
+import { FC, useEffect, useState } from 'react'
 import { Text, View } from 'react-native'
 
+import { useAuth } from '@/hooks/useAuth'
+
 const Navigation: FC = () => {
+	const { user } = useAuth()
+
+	const [currentRoute, setCurrentRoute] = useState<string | undefined>(
+		undefined
+	)
+
+	const navRef = useNavigationContainerRef()
+
+	useEffect(() => {
+		setCurrentRoute(navRef.getCurrentRoute()?.name)
+
+		const listener = navRef.addListener('state', () =>
+			setCurrentRoute(navRef.getCurrentRoute()?.name)
+		)
+
+		return () => {
+			navRef.removeListener('state', listener)
+		}
+	}, [])
 	return (
 		<View>
 			<Text>Navigation</Text>
